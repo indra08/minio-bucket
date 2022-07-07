4ff0849b6f3aeaf373d382eaa58c90ea559094b8
indramubarok <maulana.husni@gmedia.co.id>
2022-07-07 10:54:42 +0700

    update with link

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const router = express.Router();
 const port = 3000;
 const bodyParser = require("body-parser");
+const https = require('https'); 
+const http = require('http'); 
+const fs = require('fs');
+const axios = require('axios');
 
 //Here we are configuring express to use body-parser as middle-ware.
 var cors = require('cors');
@@ -71,6 +75,56 @@ app.post('/upload/:bucket', function(req, res) {
                 message: 'File uploaded successfully'
               }
           });
+        }
+    });
+});
+
+app.post('/upload_link/:bucket', async function(req, res) {
+  
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    if(req.params.bucket == null){
+        res.send({
+            status: 404,
+            message: 'bucket is empty, plese fill after link'
+        });
+    }
+
+    let link = req.body.link;  
+    let data = link.split('?')[0];
+    const ext = data
+    .split('.')
+    .pop();
+
+    const file = new Date().getTime() +"."+ ext;
+    var fileName = file;
+    
+    const response = await axios.get(
+        link,
+        { responseType: 'arraybuffer' }
+    );
+    const buffer = Buffer.from(response.data, 'utf-8');
+
+    minioClient.minioClient.putObject(req.params.bucket, fileName, buffer, function(error, etag) {
+
+        if(error) {
+            res.send({
+                status: 404,
+                message: 'No file uploaded'
+            });
+        }else{
+
+            const fileUploaded = `https://${minioClient.endPoint}/${req.params.bucket}/${fileName}`;
+            res.send({
+                response:{
+                    url : fileUploaded
+                },
+                metadata:{
+                status: 200,
+                message: 'File uploaded successfully'
+                }
+            });
         }
     });
 });
